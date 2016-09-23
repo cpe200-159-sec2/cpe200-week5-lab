107d46d0c236f9e6d915a48034531a156eac603e
nohajime <patchara0pt002@gmail.com>
2016-09-23 16:11:49 +0700

    xxxx

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -2,38 +2,90 @@ package cpe200;
 
 
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand;
+    private Operand secondOperand;
+    private double firstNum,secondNum;
 
     public BinaryCalculator() {
+        setFirstOperand(new Operand('0'));
+        setSecondOperand(new Operand('0'));
     }
 
     public void setFirstOperand(Operand operand) {
+        this.firstOperand = operand;
+        firstNum = Double.parseDouble(firstOperand.getOperand());
     }
 
 
     public void setSecondOperand(Operand operand) {
+        this.secondOperand = operand;
+        secondNum = Double.parseDouble(secondOperand.getOperand());
     }
 
     public String add() throws RuntimeException {
-        return null;
+        if(firstNum < 0 || secondNum < 0){
+            throw new RuntimeException("Error");
+        }else {
+            String sum = new Double(firstNum + secondNum).toString();
+            return sum.indexOf(".") < 0 ? sum : sum.replaceAll("0*$", "").replaceAll("\\.$", "");
+        }
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        if(firstNum < 0 || secondNum < 0){
+            throw new RuntimeException("Error");
+        } else {
+            Double sumdb = firstNum - secondNum;
+            String sum;//= new Double(firstNum-secondNum).toString();
+            sum = String.format("%.5f", sumdb);
+
+            return sum.indexOf(".") < 0 ? sum : sum.replaceAll("0*$", "").replaceAll("\\.$", "");
+        }
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        if(firstNum < 0 || secondNum < 0){
+            throw new RuntimeException("Error");
+        }else{
+            String sum = new Double(firstNum*secondNum).toString();
+            return sum.indexOf(".") < 0 ? sum : sum.replaceAll("0*$","").replaceAll("\\.$","");
+        }
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        if(firstNum < 0 || secondNum < 0){
+            throw new RuntimeException("Error");
+        }else{
+            if(secondNum==0) throw new ArithmeticException();
+            Double sumdb = firstNum/secondNum;
+
+            String sum = new Double(firstNum/secondNum).toString();
+            sum = String.format("%.5f",sumdb);
+            return sum.indexOf(".") < 0 ? sum : sum.replaceAll("0*$","").replaceAll("\\.$","");
+        }
+
     }
 
     public String power() throws RuntimeException {
-        return null;
+        if(firstNum < 0 || secondNum < 0){
+            throw new RuntimeException("Error");
+        }else{
+            double power = 1;
+            for(int i=0;i<secondNum;i++){
+                power *= firstNum;
+            }
+            String sum = new Double(power).toString();
+
+            return sum.indexOf(".") < 0 ? sum : sum.replaceAll("0*$","").replaceAll("\\.$","");
+        }
     }
 
+    public Operand getFirstOperand() {
+        return firstOperand;
+    }
+
+    public Operand getSecondOperand() {
+        return secondOperand;
+    }
 }
