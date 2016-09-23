3845596f333cb8af7721629005d5c5c173200fab
MisterMu <painter.suwannawat@gmail.com>
2016-09-23 16:24:49 +0700

    Complete Users

--- lab5_2/src/cpe200/Users.java
+++ lab5_2/src/cpe200/Users.java
@@ -9,42 +9,76 @@ import java.util.ArrayList;
 public class Users {
     public ArrayList<User> userList;
 
+    public Users()
+    {
+        userList = new ArrayList<User>();
+    }
+
     public void addUser(User user)
     {
+        userList.add(user);
     }
 
     public void addUser(String userName, String password)
     {
+        User u = new User();
+        u.setUserName(userName);
+        u.setPassword(password);
+        userList.add(u);
     }
 
     public void deleteUser(User user)
     {
-
+        if (!exists(user))
+        {
+            throw new RuntimeException();
+        }
+        userList.remove(user);
     }
 
     public boolean exists(User user)
     {
+        for (short i = 0; i < count(); i++)
+        {
+            if(userList.get(i) == user)
+                return true;
+        }
         return false;
     }
 
-    public boolean usernameExists(String username)
+    public boolean usernameExists(String userName)
     {
+        for (short i = 0; i < count(); i++)
+        {
+            if(userList.get(i).getUserName().equals(userName))
+                return true;
+        }
         return false;
     }
 
     /* This method should return null when the user with username is not in the list */
     public User getUserByUsername(String userName)
     {
+        for (short i = 0; i < count(); i++)
+        {
+            if(userList.get(i).getUserName().equals(userName))
+                return userList.get(i);
+        }
         return null;
     }
 
     public int count()
     {
-        return 0;
+        return userList.size();
     }
 
     public User[] getUserArray()
     {
-        return null;
+        User u[] = new User[count()];
+        for (short i = 0; i < count(); i++)
+        {
+            u[i] = userList.get(i);
+        }
+        return u;
     }
 }
