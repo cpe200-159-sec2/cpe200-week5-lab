f39797ff0999edbdf89fd54930b06215a65d611d
Woratham Kh <kabuto.boy@gmail.com>
2016-09-23 23:04:44 +0700

    edit: Operand
    Removed 'bigdec'

--- lab5_1/src/cpe200/Operand.java
+++ lab5_1/src/cpe200/Operand.java
@@ -1,24 +1,18 @@
 package cpe200;
 
-import java.math.BigDecimal;
-
 public class Operand {
     private String operand;
-    private BigDecimal bigdec;
 
     public Operand(String operand) {
         this.setOperand(operand);
-        this.setBigdec(new BigDecimal(Double.parseDouble(operand)));
     }
 
     public Operand(int operand) {
         this.setOperand(Integer.toString(operand));
-        this.setBigdec(new BigDecimal(operand));
     }
 
     public Operand(double operand) {
         this.setOperand(Double.toString(operand));
-        this.setBigdec(new BigDecimal(operand));
     }
 
     public String getOperand() {
@@ -27,13 +21,5 @@ public class Operand {
 
     public void setOperand(String operand) {
         this.operand = operand;
-    }
-
-    public BigDecimal getBigdec() {
-        return bigdec;
-    }
-
-    public void setBigdec(BigDecimal bigdec) {
-        this.bigdec = bigdec;
     }
 }
