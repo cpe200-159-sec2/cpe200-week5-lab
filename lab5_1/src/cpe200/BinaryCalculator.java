6cc733b919a5db916a955ec754e755276e6e14b1
Jeletwilight <koonjj@gmail.com>
2016-09-23 16:51:08 +0700

    Lab 5 Complete

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,69 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private BigDecimal firstOperand;
+    private BigDecimal secondOperand;
 
     public BinaryCalculator() {
     }
 
     public void setFirstOperand(Operand operand) {
+        firstOperand = new BigDecimal(operand.getOperand());
     }
 
 
     public void setSecondOperand(Operand operand) {
+        secondOperand = new BigDecimal(operand.getOperand());
     }
 
-    public String add() throws RuntimeException {
-        return null;
+    public String add() throws Exception{
+        if(firstOperand.intValue()<0||secondOperand.intValue()<0) {
+            throw new RuntimeException("Must Be Non-Negative Number");
+        }else {
+            return firstOperand.add(secondOperand).stripTrailingZeros().toString();
+        }
     }
 
-    public String subtract() throws RuntimeException {
-        return null;
+    public String subtract() throws Exception{
+        if(firstOperand.intValue()<0||secondOperand.intValue()<0) {
+            throw new RuntimeException("Must Be Non-Negative Number");
+        } else {
+            return firstOperand.subtract(secondOperand).stripTrailingZeros().toString();
+        }
     }
 
-    public String multiply() throws RuntimeException {
-        return null;
+    public String multiply() throws Exception{
+        if(firstOperand.intValue()<0||secondOperand.intValue()<0) {
+            throw new RuntimeException("Must Be Non-Negative Number");
+        } else {
+            return firstOperand.multiply(secondOperand).stripTrailingZeros().toString();
+        }
     }
 
     /* This method should throw an exception when divide by zero */
-    public String division() throws RuntimeException {
-        return null;
+    public String division() throws Exception{
+        String result;
+        if(firstOperand.intValue()<0||secondOperand.intValue()<0) {
+            throw new RuntimeException("Must Be Non-Negative Number");
+        }else {
+            if (secondOperand.intValue() == 0) {
+                throw new ArithmeticException("Divided by Zero");
+            } else {
+                result = firstOperand.divide(secondOperand, 5, BigDecimal.ROUND_HALF_UP).stripTrailingZeros().toString();
+            }
+            return result;
+        }
     }
 
-    public String power() throws RuntimeException {
-        return null;
+    public String power() throws Exception{
+        if(firstOperand.intValue()<0||secondOperand.intValue()<0) {
+            throw new RuntimeException("Must Be Non-Negative Number");
+        }else {
+            return firstOperand.pow(secondOperand.intValue()).stripTrailingZeros().toString();
+        }
     }
 
 }
--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -4,26 +4,38 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    public String userName = null;
+    public String password = "";
+    private static String oldUserName = "";
+    private static final String CHECKUSERNAME = "^[A-Za-z][A-Za-z0-9]{7,}$";
 
     public User() {
     }
 
     public String setUserName(String name) {
-        return null;
+        this.oldUserName = this.userName;
+        if(name.matches(CHECKUSERNAME)){
+            this.userName = name;
+        } else {
+            throw new RuntimeException("Incorrect Name");
+        }
+        return this.oldUserName;
     }
 
     public int setPassword(String name) {
-        return 0;
+        if((name.length()>=12)&&(name.matches(".*[a-z]+.*"))&&(name.matches(".*[A-Z]+.*"))&&(name.matches(".*[0-9]+.*"))){
+            this.password = name;
+        } else {
+            throw new RuntimeException("Incorrect Password");
+        }
+        return password.length();
     }
 
     public String getUserName() {
-
-        return null;
+        return this.userName;
     }
 
     public String getPassword() {
-        return null;
+        return this.password;
     }
 }
