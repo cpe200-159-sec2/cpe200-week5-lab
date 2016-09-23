281c9dd2a5f308ab1385b4858108cc249153b2fb
nanearnano <nanearnano@gmail.com>
2016-09-23 16:19:02 +0700

    5-1

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,77 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand;
+    private Operand secondOperand;
+    private BigDecimal first,second,sum,x;
 
     public BinaryCalculator() {
+        Operand firstOperand=new Operand(0);
+        Operand secondOperand=new Operand(0);
     }
 
     public void setFirstOperand(Operand operand) {
+        first=new BigDecimal(operand.getOperand());
     }
 
 
     public void setSecondOperand(Operand operand) {
+        second=new BigDecimal(operand.getOperand());
     }
 
     public String add() throws RuntimeException {
-        return null;
+        if(first.intValue()<0||second.intValue()<0){
+            throw  new RuntimeException("Negative value");
+        }
+        sum=first.add(second).stripTrailingZeros();
+        return sum.toString();
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        if(first.intValue()<0||second.intValue()<0){
+            throw  new RuntimeException("Negative value");
+        }
+        sum=first.subtract(second).stripTrailingZeros();
+        return sum.toString();
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        if(first.intValue()<0||second.intValue()<0){
+            throw  new RuntimeException("Negative value");
+        }
+        sum=first.multiply(second).stripTrailingZeros();
+        return sum.toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        if (second.equals(0)) {
+            throw new ArithmeticException("Divide by zero");
+        }else if(first.intValue()<0 ||second.intValue()<0){
+            throw new RuntimeException("Negative value");
+        }else {
+            return first.divide(second, 5, BigDecimal.ROUND_UP).stripTrailingZeros().toString();
+
+        }
+
     }
 
     public String power() throws RuntimeException {
-        return null;
+        if(first.intValue()<0||second.intValue()<0){
+            throw  new RuntimeException("Negative value");
+        }
+        sum=first.pow(second.intValueExact()).setScale(0);
+        return sum.toString();
     }
 
+    public Operand getFirstOperand() {
+        return firstOperand;
+    }
+
+    public Operand getSecondOperand() {
+        return secondOperand;
+    }
 }
