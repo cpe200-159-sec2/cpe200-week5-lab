ffd31e426e7f185e9078d1129566a4fbc4623523
ahar0007 <ahar0007@hotmail.co.th>
2016-09-23 16:20:38 +0700

    580610693

--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -11,19 +11,37 @@ public class User {
     }
 
     public String setUserName(String name) {
-        return null;
+        String regex = "^[A-Za-z][a-zA-Z0-9]{7,}$";
+        if(!name.matches(regex)) throw new RuntimeException();
+
+        if(name.matches(regex)) {
+            String oldName = userName;
+            userName = name;
+            return oldName;
+        }
+        else return userName;
+    }
+
+    public User(String user,String pass){
+        setUserName(user);
+        setPassword(pass);
     }
 
     public int setPassword(String name) {
-        return 0;
+        String regex = "^((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]))([A-Za-z0-9]){12,}$";
+        if (name.matches(regex))
+        {
+            password = name;
+            return password.length();
+        }
+        else return Integer.valueOf(password);
     }
 
     public String getUserName() {
-
-        return null;
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 }
