1759dcb1af930d91e69ed3a5036d0166c804605d
_iDong <dd_dong.bk@hotmail.com>
2016-09-23 17:13:19 +0700

    Complete

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,76 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private BigDecimal firstOperand;
+    private BigDecimal secondOperand;
 
     public BinaryCalculator() {
     }
 
     public void setFirstOperand(Operand operand) {
+        setFirstOperand(new BigDecimal(operand.getOperand()));
     }
 
 
     public void setSecondOperand(Operand operand) {
+        setSecondOperand(new BigDecimal(operand.getOperand()));
     }
 
     public String add() throws RuntimeException {
-        return null;
+        if (getFirstOperand().intValue() < 0.0 || getSecondOperand().intValue() < 0.0)
+            throw new RuntimeException("Nagative value");
+        String add = getFirstOperand().add(getSecondOperand()).stripTrailingZeros().toString();
+        return add;
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        if (getFirstOperand().intValue() < 0.0 || getSecondOperand().intValue() < 0.0)
+            throw new RuntimeException("Nagative value");
+        String sub = getFirstOperand().subtract(getSecondOperand()).stripTrailingZeros().toString();
+        return sub;
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        if (getFirstOperand().intValue() < 0.0 || getSecondOperand().intValue() < 0.0)
+            throw new RuntimeException("Nagative value");
+        String mul = getFirstOperand().multiply(getSecondOperand()).stripTrailingZeros().toString();
+        return mul;
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        String divi = getFirstOperand().divide(getSecondOperand(), 5, BigDecimal.ROUND_HALF_UP).stripTrailingZeros().toString();
+        if (getFirstOperand().intValue() < 0 || getSecondOperand().intValue() < 0) {
+            throw new RuntimeException("Nagative value");
+        } else if (getSecondOperand().intValue() == 0) {
+            throw new ArithmeticException("divide by zero");
+        }
+        return divi;
     }
 
     public String power() throws RuntimeException {
-        return null;
+        if (getFirstOperand().intValue() < 0.0 || getSecondOperand().intValue() < 0.0)
+            throw new RuntimeException("Nagative value");
+        String pow = getFirstOperand().pow(getSecondOperand().intValue()).stripTrailingZeros().toString();
+        return pow;
     }
 
+    public BigDecimal getFirstOperand() {
+        return firstOperand;
+    }
+
+    public void setFirstOperand(BigDecimal firstOperand) {
+        this.firstOperand = firstOperand;
+    }
+
+    public BigDecimal getSecondOperand() {
+        return secondOperand;
+    }
+
+    public void setSecondOperand(BigDecimal secondOperand) {
+        this.secondOperand = secondOperand;
+    }
 }
--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -4,26 +4,55 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName;
+    private String password;
 
     public User() {
     }
 
     public String setUserName(String name) {
-        return null;
+        String regex = "^[A-Za-z][a-zA-Z0-9]{7,}$";
+        if (name.matches(regex)) {
+            String preName = userName;
+            userName = name;
+            return preName;
+        } else {
+            throw new RuntimeException("");
+        }
     }
 
     public int setPassword(String name) {
-        return 0;
+        String regex = "^((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]))([A-Za-z0-9]){12,}$";
+        if (name.matches(regex)) {
+            password = name;
+            return name.length();
+        } else {
+            throw new RuntimeException("");
+        }
     }
 
     public String getUserName() {
 
-        return null;
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+        return password;
+    }
+
+    public String secreteGetUserName() {
+        return userName;
+    }
+
+    public void secretSetUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String secertGetPassword() {
+        return password;
+    }
+
+    public void secretSetPassword(String password) {
+        this.password = password;
     }
 }
