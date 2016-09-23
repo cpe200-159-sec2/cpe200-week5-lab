6b4a742b21ed75f90acadee4b75e29ef0142fedc
ASUS_kwpinthong <kw.pinthong@gmail.com>
2016-09-23 16:27:15 +0700

    Complete LAB1 and LAB2

--- lab5_2/src/cpe200/Users.java
+++ lab5_2/src/cpe200/Users.java
@@ -9,42 +9,63 @@ import java.util.ArrayList;
 public class Users {
     public ArrayList<User> userList;
 
+    public Users(){
+        userList = new ArrayList<User>();
+    }
+
     public void addUser(User user)
     {
+        this.userList.add(user);
     }
 
     public void addUser(String userName, String password)
     {
+        User newUser = new User();
+        newUser.setUserName(userName);
+        newUser.setPassword(password);
+
+        if(newUser != null){
+            this.userList.add(newUser);
+        }
     }
 
     public void deleteUser(User user)
     {
-
+        if(exists(user)){
+            this.userList.remove(user);
+        }else{
+            throw new RuntimeException();
+        }
     }
 
     public boolean exists(User user)
     {
-        return false;
+        return userList.contains(user);
     }
 
     public boolean usernameExists(String username)
     {
-        return false;
+        return userList.contains(username);
     }
 
     /* This method should return null when the user with username is not in the list */
     public User getUserByUsername(String userName)
     {
+        for(int i=0; i < userList.size() ; i++){
+            if(userList.get(i).getUserName().equalsIgnoreCase(userName)){
+                return userList.get(i);
+            }
+        }
         return null;
     }
 
     public int count()
     {
-        return 0;
+        return userList.size();
     }
 
     public User[] getUserArray()
     {
-        return null;
+        return userList.toArray(new User[count()]);
     }
 }
