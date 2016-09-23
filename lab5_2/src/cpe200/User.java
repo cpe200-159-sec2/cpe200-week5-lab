3a639e8e394177698577f04780d043b3bbcc3802
cwm <warakornchamp@gmail.com>
2016-09-23 15:41:17 +0700

    Finish

--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -3,27 +3,41 @@ package cpe200;
 /**
  * Created by pruet on 6/9/2559.
  */
-public class User {
-    public String userName;
-    public String password;
 
-    public User() {
+    public class User {
+        private String userName;
+        private String password;
+
+        public User() {
+        }
+
+        public String setUserName(String name) {
+            String check = "^[A-Za-z][A-Za-z0-9]{7,}$";
+            if (name.matches(check)) {
+                String tmp = userName;
+                userName = name;
+                return tmp;
+            } else {
+                throw new RuntimeException("Invalid name");
+            }
+        }
+
+        public int setPassword(String name) {
+            String check = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{12,}$";
+            if (name.matches(check)) {
+                password = name;
+                return password.length();
+            } else {
+                throw new RuntimeException("Invalid password");
+            }
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public String getPassword() {
+            return password;
+        }
     }
 
-    public String setUserName(String name) {
-        return null;
-    }
-
-    public int setPassword(String name) {
-        return 0;
-    }
-
-    public String getUserName() {
-
-        return null;
-    }
-
-    public String getPassword() {
-        return null;
-    }
-}
--- lab5_2/src/cpe200/Users.java
+++ lab5_2/src/cpe200/Users.java
@@ -7,44 +7,67 @@ import java.util.ArrayList;
  * Created by pruet on 12/9/2559.
  */
 public class Users {
-    public ArrayList<User> userList;
+    private ArrayList<User> userList;
 
-    public void addUser(User user)
-    {
+    public Users() {
+        userList = new ArrayList<User>();
     }
 
-    public void addUser(String userName, String password)
-    {
+    public void addUser(User user) {
+        userList.add(user);
     }
 
-    public void deleteUser(User user)
-    {
-
+    public void addUser(String userName, String password) {
+        User user = new User();
+        user.setUserName(userName);
+        user.setPassword(password);
+        userList.add(user);
     }
 
-    public boolean exists(User user)
-    {
-        return false;
+    public void deleteUser(User user) {
+        if(userList.contains(user)){
+            userList.remove(user);
+        }else{
+            throw new RuntimeException("User does not exists");
+        }
     }
 
-    public boolean usernameExists(String username)
-    {
+    public boolean exists(User user) {
+        return userList.contains(user);
+    }
+
+    public boolean usernameExists(String username) {
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).getUserName().equals(username)) {
+                return true;
+            }
+        }
         return false;
     }
 
     /* This method should return null when the user with username is not in the list */
-    public User getUserByUsername(String userName)
-    {
+    public User getUserByUsername(String userName) {
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).getUserName().equals(userName)) {
+                return userList.get(i);
+            }
+        }
         return null;
     }
 
-    public int count()
-    {
-        return 0;
+    public int count() {
+        return userList.size();
     }
 
-    public User[] getUserArray()
-    {
-        return null;
+    public User[] getUserArray() {
+        return userList.toArray(new User[userList.size()]);
+    }
+
+    public ArrayList<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(ArrayList<User> userList) {
+        this.userList = userList;
     }
 }
