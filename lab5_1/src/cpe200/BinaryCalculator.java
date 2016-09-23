af4ee1622a90b533dc6bb4db3beb2e2def34b130
supakarn39 <supakarn39@hotmail.com>
2016-09-24 02:54:56 +0700

    Finished!

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,66 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private BigDecimal firstOperand;
+    private BigDecimal secondOperand;
 
     public BinaryCalculator() {
     }
 
     public void setFirstOperand(Operand operand) {
+        firstOperand = new BigDecimal(operand.getOperand());
     }
 
 
     public void setSecondOperand(Operand operand) {
+        secondOperand = new BigDecimal(operand.getOperand());
     }
 
     public String add() throws RuntimeException {
-        return null;
+        if(firstOperand.doubleValue()<0||secondOperand.doubleValue()<0){
+            throw new RuntimeException("Negative value");
+        }else{
+            return firstOperand.add(secondOperand).stripTrailingZeros().toString();
+        }
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        if(firstOperand.doubleValue()<0||secondOperand.doubleValue()<0){
+            throw new RuntimeException("Negative value");
+        }else{
+            return firstOperand.subtract(secondOperand).stripTrailingZeros().toString();
+        }
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        if(firstOperand.doubleValue()<0||secondOperand.doubleValue()<0){
+            throw new RuntimeException("Negative value");
+        }else{
+            return firstOperand.multiply(secondOperand).stripTrailingZeros().toString();
+        }
     }
 
     /* This method should throw an exception when divide by zero */
-    public String division() throws RuntimeException {
-        return null;
+    public String division() throws ArithmeticException {
+        if(secondOperand.toString() == "0"){
+            throw new ArithmeticException("divide by zero");
+        }else if(firstOperand.doubleValue()<0||secondOperand.doubleValue()<0) {
+            throw new RuntimeException("Negative value");
+        }else{
+            return firstOperand.divide(secondOperand,5, RoundingMode.HALF_UP).stripTrailingZeros().toString();
+        }
     }
 
     public String power() throws RuntimeException {
-        return null;
+        if(firstOperand.doubleValue()<0||secondOperand.doubleValue()<0){
+            throw new RuntimeException("Negative value");
+        }else{
+            return firstOperand.pow(secondOperand.intValue()).stripTrailingZeros().toString();
+        }
     }
 
 }
--- lab5_1/src/cpe200/Operand.java
+++ lab5_1/src/cpe200/Operand.java
@@ -1,14 +1,26 @@
 package cpe200;
 
+
 public class Operand {
     private String operand;
 
     public Operand(String operand) {
+        this.operand = operand;
     }
 
     public Operand(int operand) {
+        this.operand = Integer.toString(operand);
     }
 
     public Operand(double operand) {
+        this.operand = Double.toString(operand);
+    }
+
+    public String getOperand(){
+        return this.operand;
+    }
+
+    public void setOperand(Operand newoperand){
+        this.operand = newoperand.getOperand();
     }
 }
