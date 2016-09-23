4aaf2f71cc78d5b6e7b5d02087bf09bd7ca0fb24
tiw3393 <tam3393@hotmail.co.th>
2016-09-23 16:35:32 +0700

    มาหว่ะ

--- lab5_1/src/cpe200/Operand.java
+++ lab5_1/src/cpe200/Operand.java
@@ -3,12 +3,26 @@ package cpe200;
 public class Operand {
     private String operand;
 
-    public Operand(String operand) {
+    public Operand(String operand)
+    {
+        this.setOperand(operand);
     }
 
-    public Operand(int operand) {
+    public Operand(int operand)
+    {
+        this.setOperand(Integer.toString(operand));
     }
 
-    public Operand(double operand) {
+    public Operand(double operand)
+    {
+        this.setOperand(Double.toString(operand));
+    }
+
+    public String getOperand() {
+        return operand;
+    }
+
+    public void setOperand(String operand) {
+        this.operand = operand;
     }
 }
--- lab5_2/src/cpe200/Users.java
+++ lab5_2/src/cpe200/Users.java
@@ -7,44 +7,61 @@ import java.util.ArrayList;
  * Created by pruet on 12/9/2559.
  */
 public class Users {
-    public ArrayList<User> userList;
+    public ArrayList<User> userList = new ArrayList<User>();
 
     public void addUser(User user)
     {
+        this.userList.add(user);
     }
 
     public void addUser(String userName, String password)
     {
+        this.userList.add(new User(userName,password));
     }
 
     public void deleteUser(User user)
     {
-
+        if(count() == 0)throw new RuntimeException("Don't have User");
+            this.userList.remove(user);
     }
 
     public boolean exists(User user)
     {
-        return false;
+        if(userList.contains(user)){
+            return true;
+        }else {
+             return false;
+        }
     }
 
     public boolean usernameExists(String username)
     {
+       for(int i = 0;i < userList.size();i++){
+            if(userList.get(i).getUserName().equalsIgnoreCase(username)){
+                return true;
+            }
+        }
         return false;
     }
 
     /* This method should return null when the user with username is not in the list */
     public User getUserByUsername(String userName)
     {
+        for(int i = 0;i <userList.size();i++){
+            if(userList.get(i).getUserName().equalsIgnoreCase(userName)){
+                return userList.get(i);
+            }
+        }
         return null;
     }
 
     public int count()
     {
-        return 0;
+        return this.userList.size();
     }
 
     public User[] getUserArray()
     {
-        return null;
+        return this.userList.toArray(new User[count()]);
     }
 }
