79b9b9dd1633f94fefaf9d5bcda54c202b23d794
PoommipatSe <poommipat.setthakorn@gmail.com>
2016-09-23 16:20:45 +0700

    Update Operand.java

--- lab5_1/src/cpe200/Operand.java
+++ lab5_1/src/cpe200/Operand.java
@@ -3,12 +3,22 @@ package cpe200;
 public class Operand {
     private String operand;
 
-    public Operand(String operand) {
+    public Operand(String operand)
+    {
+        this.operand = operand;
     }
 
-    public Operand(int operand) {
+    public Operand(int operand)
+    {
+        this.operand = Integer.toString(operand);
     }
 
-    public Operand(double operand) {
+    public Operand(double operand)
+    {
+        this.operand = Double.toString(operand);
+    }
+
+    public String getOperand() {
+        return operand;
     }
 }
