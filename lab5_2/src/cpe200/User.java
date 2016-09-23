8258cf1a6721c2704d1a1fa347acd476f0f1773f
MisterMu <painter.suwannawat@gmail.com>
2016-09-23 16:15:56 +0700

    Complete User

--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -4,26 +4,39 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName;
+    private String password;
+
+    private static final String userREGEX = "^([A-Za-z])([A-Za-z0-9]{7,})$";
+    private static final String passREGEX = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])([A-Za-z0-9]){12,}$";
 
     public User() {
     }
 
     public String setUserName(String name) {
-        return null;
+        String tmp_str = userName;
+        if (name.matches(userREGEX))
+        {
+            userName = name;
+            return tmp_str;
+        }
+        throw new RuntimeException();
     }
 
-    public int setPassword(String name) {
-        return 0;
+    public int setPassword(String pass) {
+        if (pass.matches(passREGEX))
+        {
+            password = pass;
+            return pass.length();
+        }
+        throw new RuntimeException();
     }
 
     public String getUserName() {
-
-        return null;
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 }
