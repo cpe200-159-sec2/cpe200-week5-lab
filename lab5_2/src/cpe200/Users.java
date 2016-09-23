145b63868aa3c418728e118187c29e02f5465f2f
Kaiwit Maneewong <Kaiwit.maneewong@gmail.com>
2016-09-23 16:26:26 +0700

    580610620

--- lab5_2/src/cpe200/Users.java
+++ lab5_2/src/cpe200/Users.java
@@ -1,50 +1,71 @@
 package cpe200;
 
-
 import java.util.ArrayList;
 
 /**
  * Created by pruet on 12/9/2559.
  */
 public class Users {
-    public ArrayList<User> userList;
+    private ArrayList<User> userList;
 
-    public void addUser(User user)
-    {
+    public Users(){
+        userList = new ArrayList<User>();
+    }
+    public void addUser(User user) {
+        getUserList().add(user);
     }
 
-    public void addUser(String userName, String password)
-    {
+    public void addUser(String userName, String password) {
+        User user = new User(userName,password);
+        getUserList().add(user);
     }
 
-    public void deleteUser(User user)
-    {
-
+    public void deleteUser(User user) {
+        if(count() == 0) throw new RuntimeException();
+        getUserList().remove(user);
     }
 
-    public boolean exists(User user)
-    {
+    public boolean exists(User user) {
+        if(userList.contains(user)){
+            return  true;
+        }
         return false;
     }
 
-    public boolean usernameExists(String username)
-    {
+    public boolean usernameExists(String username) {
+        for(int i=0;i>userList.size();i++){
+            if (username.equals(userList.get(i).getUserName())) {
+                return true;
+            }
+        }
         return false;
     }
 
     /* This method should return null when the user with username is not in the list */
-    public User getUserByUsername(String userName)
-    {
+    public User getUserByUsername(String userName) {
+        String check;
+        for(int i=0;i<userList.size();i++){
+            check =userList.get(i).getUserName();
+            if (check.equals(userName)) {
+                return userList.get(i);
+            }
+        }
         return null;
     }
 
-    public int count()
-    {
-        return 0;
+    public int count() {
+        return userList.size();
     }
 
-    public User[] getUserArray()
-    {
-        return null;
+    public User[] getUserArray() {
+        return userList.toArray(new User[userList.size()]);
+    }
+
+    public ArrayList<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(ArrayList<User> userList) {
+        this.userList = userList;
     }
 }
