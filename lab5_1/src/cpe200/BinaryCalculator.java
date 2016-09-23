df9a37d2708d3e5f13da42151b15ce0fccab678f
totaythatchayut <thatchayut.tay@gmail.com>
2016-09-23 16:00:25 +0700

    LAB 1 : DONE

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,97 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand;
+    private Operand secondOperand;
 
     public BinaryCalculator() {
     }
 
     public void setFirstOperand(Operand operand) {
+        firstOperand=operand;
     }
 
 
     public void setSecondOperand(Operand operand) {
+        secondOperand=operand;
     }
 
-    public String add() throws RuntimeException {
-        return null;
+    public String add() {
+        if((Double.parseDouble(firstOperand.getOperand()) < 0 || Double.parseDouble(secondOperand.getOperand()) < 0))
+        {
+            throw new RuntimeException("negative values are not allowed.");
+        }
+        else
+        {
+            BigDecimal Num1= new BigDecimal(firstOperand.getOperand());
+            BigDecimal Num2 = new BigDecimal(secondOperand.getOperand());
+            Num1 = Num1.add(Num2).stripTrailingZeros();
+            return Num1.toString();
+        }
     }
 
-    public String subtract() throws RuntimeException {
-        return null;
+    public String subtract() {
+        if((Double.parseDouble(firstOperand.getOperand()) < 0 || Double.parseDouble(secondOperand.getOperand()) < 0))
+        {
+            throw new RuntimeException("negative values are not allowed.");
+        }
+        else
+        {
+            BigDecimal Num1 = new BigDecimal(firstOperand.getOperand());
+            BigDecimal Num2 = new BigDecimal(secondOperand.getOperand());
+            Num1 = Num1.subtract(Num2).stripTrailingZeros();
+            return Num1.toString();
+        }
     }
 
-    public String multiply() throws RuntimeException {
-        return null;
+    public String multiply() {
+        if((Double.parseDouble(firstOperand.getOperand()) < 0 || Double.parseDouble(secondOperand.getOperand()) < 0))
+        {
+            throw new RuntimeException("negative values are not allowed.");
+        }
+        else
+        {
+            BigDecimal Num1 = new BigDecimal((firstOperand.getOperand()));
+            BigDecimal Num2 = new BigDecimal((secondOperand.getOperand()));
+            Num1 = Num1.multiply(Num2).stripTrailingZeros();
+            return Num1.toString();
+        }
     }
 
     /* This method should throw an exception when divide by zero */
-    public String division() throws RuntimeException {
-        return null;
+    public String division()  {
+        if((Double.parseDouble(firstOperand.getOperand()) < 0 || Double.parseDouble(secondOperand.getOperand()) < 0))
+        {
+            throw new RuntimeException("negative values are not allowed.");
+        }
+        else if(Double.parseDouble(secondOperand.getOperand())==0)
+        {
+            throw new ArithmeticException("Divided by zero");
+        }
+        else
+        {
+            BigDecimal Num1 = new BigDecimal(firstOperand.getOperand());
+            BigDecimal Num2 = new BigDecimal(secondOperand.getOperand());
+            Num1 = Num1.divide(Num2,5,BigDecimal.ROUND_HALF_UP).stripTrailingZeros();
+            return Num1.toString();
+        }
     }
 
-    public String power() throws RuntimeException {
-        return null;
+    public String power() {
+        if((Double.parseDouble(firstOperand.getOperand()) < 0 || Double.parseDouble(secondOperand.getOperand()) < 0))
+        {
+            throw new RuntimeException("negative values are not allowed.");
+        }
+        else
+        {
+            BigDecimal Num1 = new BigDecimal(firstOperand.getOperand());
+            BigDecimal Num2 = new BigDecimal(secondOperand.getOperand());
+            Num1 = Num1.pow(Num2.intValue()).stripTrailingZeros();
+            return Num1.toString();
+        }
     }
 
 }
--- lab5_1/src/cpe200/Operand.java
+++ lab5_1/src/cpe200/Operand.java
@@ -4,11 +4,35 @@ public class Operand {
     private String operand;
 
     public Operand(String operand) {
+        setOperand(operand);
     }
 
     public Operand(int operand) {
+        setOperand(operand);
     }
 
     public Operand(double operand) {
+        setOperand(operand);
+    }
+
+
+    public void setOperand(int operand)
+    {
+        this.operand=Integer.toString(operand);
+    }
+
+    public void setOperand(double operand)
+    {
+        this.operand=Double.toString(operand);
+    }
+
+    public String getOperand()
+    {
+        return this.operand;
+    }
+
+
+    public void setOperand(String operand) {
+        this.operand = operand;
     }
 }
