343e89b3f4f801215eef3e519b09e0d37cf35477
ASUS_kwpinthong <kw.pinthong@gmail.com>
2016-09-23 16:25:43 +0700

    Complete LAB1 and LAB2 Ver2

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,84 @@
 package cpe200;
 
 
+import sun.nio.cs.ext.Big5_HKSCS;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand;
+    private Operand secondOperand;
 
     public BinaryCalculator() {
     }
 
     public void setFirstOperand(Operand operand) {
+        firstOperand = operand;
     }
 
-
     public void setSecondOperand(Operand operand) {
+        secondOperand = operand;
     }
 
     public String add() throws RuntimeException {
-        return null;
+        BigDecimal one = new BigDecimal(firstOperand.getOperand());
+        BigDecimal two = new BigDecimal(secondOperand.getOperand());
+
+        if(one.signum()==-1||two.signum()==-1){
+            throw new RuntimeException();
+        }else{
+            return one.add(two).stripTrailingZeros().toString();
+        }
+
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        BigDecimal one = new BigDecimal(firstOperand.getOperand());
+        BigDecimal two = new BigDecimal(secondOperand.getOperand());
+
+        if(one.signum()==-1||two.signum()==-1){
+            throw new RuntimeException();
+        }else{
+            return one.subtract(two).stripTrailingZeros().toString();
+        }
+
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        BigDecimal one = new BigDecimal(firstOperand.getOperand());
+        BigDecimal two = new BigDecimal(secondOperand.getOperand());
+
+        if(one.signum()==-1||two.signum()==-1){
+            throw new RuntimeException();
+        }else{
+            return one.multiply(two).stripTrailingZeros().toString();
+        }
+
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        BigDecimal one = new BigDecimal(firstOperand.getOperand());
+        BigDecimal two = new BigDecimal(secondOperand.getOperand());
+
+        if(one.signum()==-1||two.signum()==-1){
+            throw new RuntimeException();
+        }else{
+            return one.divide(two,5, RoundingMode.HALF_UP).stripTrailingZeros().toString();
+        }
+
     }
 
     public String power() throws RuntimeException {
-        return null;
-    }
+        BigDecimal one = new BigDecimal(firstOperand.getOperand());
+        BigDecimal two = new BigDecimal(secondOperand.getOperand());
 
+        if(one.signum() == -1 || two.signum() == -1){
+            throw new RuntimeException();
+        }else{
+            return one.pow(two.intValueExact()).stripTrailingZeros().toString();
+        }
+
+    }
 }
