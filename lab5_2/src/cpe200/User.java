fbf793a699e214f7859534de975325fa82140d75
Kittitat <KittitatBoonk@gmail.com>
2016-09-23 14:49:38 +0700

    Finnish Lab 5

--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -4,26 +4,39 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName;
+    private String password;
 
     public User() {
     }
-
-    public String setUserName(String name) {
-        return null;
+    public User(String user,String pass){
+        setUserName(user);
+        setPassword(pass);
+    }
+    public String setUserName(String name)
+    {
+        String preUsername = getUserName();
+        if(name.matches("^([A-Za-z])([A-Za-z0-9]{7,})$")){
+            this.userName = name;
+            return preUsername;
+        }
+        throw new RuntimeException("ERROR");
     }
 
-    public int setPassword(String name) {
-        return 0;
+    public int setPassword(String name)
+    {
+        if(name.matches("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])([A-Za-z0-9]){12,}$")){
+            this.password = name;
+            return this.password.length();
+        }
+        throw new RuntimeException("ERROR");
     }
 
     public String getUserName() {
-
-        return null;
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 }
