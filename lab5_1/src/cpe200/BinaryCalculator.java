e6e30d29c8c624abd9af68c9d403ffe469a8f9ef
Nattawut Khamchai <nattawut.khamchai@gmail.com>
2016-09-23 23:02:20 +0700

    Lab 5.1 Complete

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,73 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private BigDecimal firstOperand;
+    private BigDecimal secondOperand;
+
+    public BigDecimal getFirstOperand() {
+        return firstOperand;
+    }
+
+    public void setFirstOperand(BigDecimal firstOperand) {
+        this.firstOperand = firstOperand;
+    }
+
+    public BigDecimal getSecondOperand() {
+        return secondOperand;
+    }
+
+    public void setSecondOperand(BigDecimal secondOperand) {
+        this.secondOperand = secondOperand;
+    }
 
     public BinaryCalculator() {
     }
 
     public void setFirstOperand(Operand operand) {
+        setFirstOperand(new BigDecimal(operand.getOperand()));
     }
 
-
     public void setSecondOperand(Operand operand) {
+        setSecondOperand(new BigDecimal(operand.getOperand()));
     }
 
     public String add() throws RuntimeException {
-        return null;
+        if (getSecondOperand().intValue() < 0 || getFirstOperand().intValue() < 0) {
+            throw new RuntimeException("ERROR");
+        }
+        return getFirstOperand().add(getSecondOperand()).stripTrailingZeros().toString();
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        if (getSecondOperand().intValue() < 0 || getFirstOperand().intValue() < 0) {
+            throw new RuntimeException("ERROR");
+        }
+        return getFirstOperand().subtract(getSecondOperand()).stripTrailingZeros().toString();
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        if (getSecondOperand().intValue() < 0 || getFirstOperand().intValue() < 0) {
+            throw new RuntimeException("ERROE");
+        }
+        return getFirstOperand().multiply(getSecondOperand()).stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        if (getSecondOperand().toString() == "0" || getSecondOperand().intValue() < 0 || getFirstOperand().intValue() < 0) {
+            throw new RuntimeException("ERROR");
+        }
+        return getFirstOperand().divide(getSecondOperand(), 5, BigDecimal.ROUND_CEILING).stripTrailingZeros().toString();
     }
 
     public String power() throws RuntimeException {
-        return null;
+        int powered = getFirstOperand().intValue();
+        if (getSecondOperand().intValue() < 0 || getFirstOperand().intValue() < 0) {
+            throw new RuntimeException("ERROR");
+        }
+        return getFirstOperand().stripTrailingZeros().pow(getSecondOperand().intValue()).stripTrailingZeros().toString();
     }
-
 }
--- lab5_1/src/cpe200/Operand.java
+++ lab5_1/src/cpe200/Operand.java
@@ -1,14 +1,21 @@
 package cpe200;
 
+
 public class Operand {
     private String operand;
 
     public Operand(String operand) {
+        this.operand = operand;
     }
 
     public Operand(int operand) {
+        this.operand = Integer.toString(operand);
     }
 
     public Operand(double operand) {
+        this.operand = Double.toString(operand);
+    }
+    public String getOperand(){
+        return operand;
     }
 }
