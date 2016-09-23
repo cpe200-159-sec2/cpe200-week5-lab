cd8dd7a1fd591f273ca4abb585b6335dbbe85e35
GEST <naluedomgest@gmail.com>
2016-09-23 16:29:34 +0700

    Complete

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,59 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private BigDecimal firstOperand;
+    private BigDecimal secondOperand;
 
     public BinaryCalculator() {
+        firstOperand  = new BigDecimal(0);
+        secondOperand = new BigDecimal(0);
     }
 
     public void setFirstOperand(Operand operand) {
+        firstOperand = new BigDecimal(operand.getOperand());
     }
 
 
     public void setSecondOperand(Operand operand) {
+        secondOperand = new BigDecimal(operand.getOperand());
     }
 
     public String add() throws RuntimeException {
-        return null;
+        if(getFirstOperand().intValue()<0||getSecondOperand().intValue()<0)throw new RuntimeException("ERROR");
+        return  firstOperand.add(secondOperand).stripTrailingZeros().toString();
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        if(getFirstOperand().intValue()<0||getSecondOperand().intValue()<0)throw new RuntimeException("ERROR");
+        return  firstOperand.subtract(secondOperand).stripTrailingZeros().toString();
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        if(getFirstOperand().intValue()<0||getSecondOperand().intValue()<0)throw new RuntimeException("ERROR");
+        return firstOperand.multiply(secondOperand).stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        if(getSecondOperand().toString()=="0"||getSecondOperand().intValue()<0||getFirstOperand().intValue()<0){
+            throw new RuntimeException("Error");
+        }
+        return firstOperand.divide(secondOperand,5,BigDecimal.ROUND_HALF_UP).stripTrailingZeros().toString();
     }
 
     public String power() throws RuntimeException {
-        return null;
+        if(getFirstOperand().intValue()<0||getSecondOperand().intValue()<0)throw new RuntimeException("ERROR");
+        return firstOperand.pow(secondOperand.intValue()).stripTrailingZeros().toString();
     }
 
+    public BigDecimal getSecondOperand() {
+        return secondOperand;
+    }
+
+    public BigDecimal getFirstOperand() {
+        return firstOperand;
+    }
 }
--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -4,26 +4,37 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName;
+    private String password;
 
     public User() {
     }
 
     public String setUserName(String name) {
-        return null;
+        String PreUsername = getUserName();
+        String regax = "^[a-zA-Z][a-zA-Z0-9]{7,}$";
+        if(name.matches(regax)){
+            userName = name ;
+            return PreUsername;
+        }
+        throw new RuntimeException("Error");
     }
 
     public int setPassword(String name) {
-        return 0;
+        String regex = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{12,}$";
+        if(name.matches((regex))){
+            password = name ;
+            return password.length();
+        }
+        throw new RuntimeException("Error");
     }
 
     public String getUserName() {
 
-        return null;
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 }
